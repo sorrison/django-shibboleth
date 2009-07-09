8e28421bc919e699582c160f0cdfdddecb2f3c03
Sam Morrison <sam@vpac.org>
2009-07-09 17:14:02 +1000

    refactored register/login view

---
+++ django_shibboleth/utils.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.http import HttpResponseForbidden
+
+
+def parse_attributes(META):
+    shib_attrs = {}
+
+    for header, attr in settings.SHIB_ATTRIBUTE_MAP:
+        required, name = attr
+        a = META.get(header, None)
+        shib_attrs[name] = a
+
+        if required and not a:
+            return HttpResponseForbidden("Required attribute %s not found" % name)
+
+    return shib_attrs
--- django_shibboleth/views.py
+++ django_shibboleth/views.py
@@ -1,79 +1,41 @@
-# Create your views here.
-from django.http import HttpResponseForbidden, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.contrib.auth import login
 from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse
-from django.template import loader
 
-attribute_map = {
-"eduPersonAssurance":"HTTP_SHIB_ASSURANCE",
-"cn":"HTTP_SHIB_CN",
-"givenName":"HTTP_SHIB_GIVENNAME",
-"l":"HTTP_SHIB_L",
-"mail":"HTTP_SHIB_MAIL",
-"o":"HTTP_SHIB_O",
-"auEduPersonSharedToken":"HTTP_SHIB_SHARED_TOKEN",
-"sn":"HTTP_SHIB_SN",
-}
+from utils import parse_attributes
 
 
-def parse_attributes(request):
-    meta = request.META
+def shib_register(request, RegisterForm=None, register_template_name='shibboleth/register.html', redirect_url='/profile/'):
 
-    attributes = {}
-    attributes['idp'] = meta.get('HTTP_SHIB_IDENTITY_PROVIDER')
-    attributes['shared_token'] = meta.get(attribute_map['auEduPersonSharedToken'])
-    attributes['username'] = meta.get(attribute_map['auEduPersonSharedToken'])
-    attributes['email'] = meta.get(attribute_map['mail'])
-    attributes['cn'] = meta.get(attribute_map['cn'])
-    attributes['organisation'] = meta.get(attribute_map['o'])
-    attributes['locality'] = meta.get(attribute_map['l'])
-    attributes['first_name'] = meta.get(attribute_map['givenName']) or cn.split(' ')[0]
-    attributes['last_name'] = meta.get(attribute_map['sn']) or cn.lstrip(first_name).strip()
-
-    attributes['shared_token'] = 'G_0-_88s1CiUXmJxKPYWF8TugZI'
-    attributes['username'] = 'G_0-_88s1CiUXmJxKPYWF8TugZI'
-    return attributes
-
-
-def shib_register(request):
-
-    attrs = parse_attributes(request)
-
-    if attrs['shared_token'] == '' or \
-       attrs['email'] == '' or \
-       attrs['cn'] == '' or \
-       attrs['organisation'] == '' or \
-       attrs['locality'] == '':
-        return HttpResponseForbidden(loader.render_to_string('forbidden.html', locals(), context_instance=RequestContext(request)))
-
+    attrs = parse_attributes(request.META)
+    
     if request.method == 'POST':
-        user = User.objects.create_user(attrs['shared_token'], attrs['email'], '')
-
-        user.set_unusable_password()
-        user.first_name = attrs['first_name']
-        user.last_name = attrs['last_name']
-        user.email = attrs['email']
-        user.save()
-
-        user.backend = 'django.contrib.auth.backends.ModelBackend'
+        # Post from register_form
+        if RegisterForm:
+            form = RegisterForm(request.POST)
+            if form.is_valid:
+                form.save()
+        user = User.objects.create_user(attr[settings.SHIB_USERNAME], attr[settings.SHIB_EMAIL], '')
 
     try:
-        user = User.objects.get(username=attrs['shared_token'])
-    except:
-        return render_to_response('register.html', locals(), context_instance=RequestContext(request))
+        user = User.objects.get(username=attr[settings.SHIB_USERNAME])
+    except User.DoesNotExist:
+        if RegisterForm:
+            form = RegisterForm()
+
+        return render_to_response(register_template_name, locals(), context_instance=RequestContext(request))
 
     user.set_unusable_password()
-    user.first_name = attrs['first_name']
-    user.last_name = attrs['last_name']
-    user.email = attrs['email']
+    user.first_name = attr[settings.SHIB_FIRST_NAME]
+    user.last_name = attr[settings.SHIB_LAST_NAME]
+    user.email = attr[settings.SHIB_EMAIL]
     user.save()
 
     user.backend = 'django.contrib.auth.backends.ModelBackend'
     login(request, user)
 
-    return HttpResponseRedirect(reverse('profile_detail'))
+    return HttpResponseRedirect(redirect_url)
 
 
