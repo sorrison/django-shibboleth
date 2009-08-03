5cd7a5c12880067a693987d583b4d44846de9621
Sam Morrison <sam@vpac.org>
2009-08-03 13:22:12 +1000

    whitespace makes things pretty

--- django_shibboleth/urls.py
+++ django_shibboleth/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 # URL patterns for django_shibboleth
 
 urlpatterns = patterns('django_shibboleth.views',
-  # Add url patterns here
-    url(r'^login/$', 'shib_register', name='shib_login'),
+
+    url(r'^login/$',    'shib_register', name='shib_login'),
     url(r'^register/$', 'shib_register', name='shib_register'),
 )
