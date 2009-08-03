0ed7102fdc5068c675fcbbbc3c67ab4fef302bd6
Sam Morrison <sam@vpac.org>
2009-08-03 13:22:36 +1000

    form context, redirect working

--- django_shibboleth/views.py
+++ django_shibboleth/views.py
@@ -8,12 +8,12 @@ from django.conf import settings
 from utils import parse_attributes
 from forms import BaseRegisterForm
 
-def shib_register(request, RegisterForm=BaseRegisterForm, register_template_name='shibboleth/register.html', redirect_url=settings.LOGIN_REDIRECT_URL):
+
+def shib_register(request, RegisterForm=BaseRegisterForm, register_template_name='shibboleth/register.html'):
 
     attr = parse_attributes(request.META)
-    
-    if request.REQUEST.has_key('next'):
-        redirect_url = request.REQUEST.get('next')
+
+    redirect_url = request.REQUEST.get('next', settings.LOGIN_REDIRECT_URL)
 
     if request.method == 'POST':
         form = RegisterForm(request.POST)
@@ -23,7 +23,8 @@ def shib_register(request, RegisterForm=BaseRegisterForm, register_template_name
         user = User.objects.get(username=attr[settings.SHIB_USERNAME])
     except User.DoesNotExist:
         form = RegisterForm()
-        return render_to_response(register_template_name, locals(), context_instance=RequestContext(request))
+        context = {'form': form, 'next': redirect_url, 'shib_attrs': attr, }
+        return render_to_response(register_template_name, context, context_instance=RequestContext(request))
 
     user.set_unusable_password()
     user.first_name = attr[settings.SHIB_FIRST_NAME]
@@ -34,12 +35,7 @@ def shib_register(request, RegisterForm=BaseRegisterForm, register_template_name
     user.backend = 'django.contrib.auth.backends.ModelBackend'
     login(request, user)
 
-    if request.REQUEST.has_key('next'):
-        redirect_url = request.REQUEST.get('next')
-
-    if not redirect_to or '//' in redirect_to or ' ' in redirect_to:
-        redirect_to = settings.LOGIN_REDIRECT_URL
+    if not redirect_url or '//' in redirect_url or ' ' in redirect_url:
+        redirect_url = settings.LOGIN_REDIRECT_URL
 
     return HttpResponseRedirect(redirect_url)
-
-
