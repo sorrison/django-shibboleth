cf3151f38728af161c1d3e86e8a3c13a6ee7dc45
Sam Morrison <sam@vpac.org>
2011-03-01 15:18:57 +1100

    New version 1.0.9

--- django_shibboleth/__init__.py
+++ django_shibboleth/__init__.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with django_shibboleth  If not, see <http://www.gnu.org/licenses/>.
 
-VERSION = (1, 0, 8, 'final', 0)
+VERSION = (1, 0, 9, 'final', 0)
 
 def get_version():
     """ Return the current version"""
